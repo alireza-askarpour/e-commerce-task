3353fe933008b251c22bd30d2462974ebbd2041e
Alireza Askarpour <askarpourdev@gmail.com>
2023-01-11 15:16:56 +0330

    feat: add SEO component

---
+++ components/Shared/SEO/settings.ts
@@ -0,0 +1,8 @@
+const settings = {
+  meta: {
+    title: "Store",
+    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero in error ut! Quo, debitis fuga.",
+  },
+}
+
+export default settings
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,11 @@
+import SEO from "components/Shared/SEO"
+
 const Home = () => {
   return (
+    <>
+    <SEO title="Home" />
     <div>Home</div>
+    </>
   )
 }
 
