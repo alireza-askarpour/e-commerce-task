cbbb928e2ff29ce03b7e524374ca140d8d24b141
Alireza Askarpour <askarpourdev@gmail.com>
2023-01-12 23:23:54 +0330

    faet(home): responsive discount section

--- components/Home/Discount/index.tsx
+++ components/Home/Discount/index.tsx
@@ -1,15 +1,15 @@
 const Discount = () => {
   return (
-    <section className="container mx-auto flex gap-x-4 mt-16">
-      <div className="w-1/2 overflow-hidden rounded-3xl">
-        <img src="/images/discount-img-1.svg" className="w-full h-full object-cover rounded-3xl" />
+    <section className="container mx-auto lg:flex gap-x-4 mt-6 lg:mt-16">
+      <div className="lg:w-1/2 overflow-hidden rounded-2xl lg:rounded-3xl mb-6 lg:mb-0">
+        <img src="/images/discount-img-1.svg" className="w-full h-full object-cover rounded-2xl lg:rounded-3xl" />
       </div>
-      <div className="w-1/2 flex flex-col gap-4">
-        <div className="h-1/2 overflow-hidden rounded-3xl">
-          <img src="/images/discount-img-2.svg" className="w-full h-full object-cover rounded-3xl" />
+      <div className="lg:w-1/2 flex flex-col gap-2 lg:gap-4">
+        <div className="h-1/2 overflow-hidden rounded-2xl lg:rounded-3xl">
+          <img src="/images/discount-img-2.svg" className="w-full h-full object-cover rounded-2xl lg:rounded-3xl" />
         </div>
-        <div className="h-1/2 overflow-hidden rounded-3xl">
-          <img src="/images/discount-img-3.svg" className="w-full h-full object-cover rounded-3xl" />
+        <div className="h-1/2 overflow-hidden rounded-2xl lg:rounded-3xl">
+          <img src="/images/discount-img-3.svg" className="w-full h-full object-cover rounded-2xl lg:rounded-3xl" />
         </div>
       </div>
     </section>
--- constants/download.ts
+++
@@ -1,5 +0,0 @@
-export const DOWNLOAD_LIST = [
-  { title: "دانلود از بازار", icon: "/images/bazar.svg" },
-  { title: "دانلود از مایکت", icon: "/images/myket.svg" },
-  { title: "دانلود مستقیم", icon: "/images/android.svg" },
-]
