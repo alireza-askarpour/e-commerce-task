e539ef511a485312cf463a5abd23b909315bfa7c
Alireza Askarpour <askarpourdev@gmail.com>
2023-01-12 22:39:02 +0330

    feat: optimization button components

--- components/Shared/Button/index.tsx
+++ components/Shared/Button/index.tsx
@@ -15,29 +15,18 @@ const Button = ({ children, className, disabled, href, onClick, type }: IProps) 
     onClick: handleClickButton,
     disabled: disabled,
     className: classNames(
-      "appearance-none relative text-center inline-block rounded-full overflow-hidden cursor-pointer select-none transition duration-150 ease-in-out transform border border-transparent px-[24px] py-2.5 text-white",
+      "appearance-none relative text-center inline-block bg-button-primary rounded-full overflow-hidden cursor-pointer select-none transition duration-150 ease-in-out transform border border-transparent px-5 py-2.5 text-white",
       disabled && "cursor-not-allowed opacity-75",
       className,
     ),
   }
 
   return href ? (
-    <Link
-      href={href}
-      {...elementProps}
-      style={{
-        background: "linear-gradient(252.18deg, #D72339 0.28%, #F23B51 93.8%)",
-      }}
-    >
+    <Link href={href} {...elementProps}>
       <span>{children}</span>
     </Link>
   ) : (
-    <button
-      {...elementProps}
-      style={{
-        background: "linear-gradient(252.18deg, #D72339 0.28%, #F23B51 93.8%)",
-      }}
-    >
+    <button {...elementProps}>
       <span>{children}</span>
     </button>
   )
