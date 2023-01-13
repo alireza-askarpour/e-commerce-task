19a5096f61288d194a5242481f06f339f4aef4ce
Alireza Askarpour <askarpourdev@gmail.com>
2023-01-13 13:09:09 +0330

    refactor: optimization icon components

--- components/Shared/Icon/index.tsx
+++ components/Shared/Icon/index.tsx
@@ -12,11 +12,11 @@ export default function Icon({ name, className, color = "gray", size = 24 }: IPr
   if (!icons[name]) throw new Error(`Cannot find icon ${name}`)
 
   const IconElement = icons[name]
-  const iconColor = colors?.[color]
+  const fill = colors?.[color]
 
   return (
     <svg
-      fill={iconColor}
+      fill={fill}
       width={size}
       height={size}
       aria-label={name}
@@ -24,7 +24,7 @@ export default function Icon({ name, className, color = "gray", size = 24 }: IPr
       viewBox="0 0 24 24"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <IconElement stopColor={iconColor} />
+      <IconElement />
     </svg>
   )
 }
