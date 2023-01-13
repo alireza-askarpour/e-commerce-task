4fd83af346ca2300d3324fa8253b54c2e42f2d76
Alireza Askarpour <askarpourdev@gmail.com>
2023-01-13 11:28:05 +0330

    refactor: add effect for search input components

--- components/Shared/SearchInput/index.tsx
+++ components/Shared/SearchInput/index.tsx
@@ -4,9 +4,17 @@ import { classNames } from "utils"
 
 const SearchInput = ({ placeholder, className }: IProps) => {
   return (
-    <div className={classNames("flex items-center justify-end rounded-full bg-[#F7F7F7]", className)}>
-      <input type="text" placeholder={placeholder} className="flex-1 outline-none pr-3 bg-inherit rounded-full" />
-      <IconButton size="lg">
+    <div className={classNames("relative h-12 rounded-full bg-gray-7", className)}>
+      <input
+        type="text"
+        placeholder={placeholder}
+        className="absolute top-0 right-0 bottom-0 left-0 pl-12 outline-none pr-3 bg-inherit rounded-full transition-all duration-300 border border-transparent border-1 border-gray-7 hover:placeholder:text-gray-2 hover:border-gray-300 focus:placeholder:text-gray-4 focus:border-red"
+      />
+      <IconButton
+        size="lg"
+        className="z-10 absolute left-[2px] top-[2px] bottom-[2px]"
+        style={{ width: "44px", height: "44px" }}
+      >
         <Icon name="search-normal" size={20} />
       </IconButton>
     </div>
